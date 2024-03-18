fd24adcde89c9fec1fcea842baa7ed422cb2035e
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2024-03-18 17:59:12 +0100

    r - use CharacterSet newlines instead of hardcoding newline + use components String's method and return an actual Array of String instead of a Substring (since you'll need to convert that again).

--- Sources/Characters/LineBreakCharacter.swift
+++ Sources/Characters/LineBreakCharacter.swift
@@ -2,10 +2,12 @@ import Foundation
 
 /// Wrapper around a character which is used to break a line.
 struct LineBreakCharacter {
-    fileprivate static let _character: Character = "\n"
+//    fileprivate static let _character: Character = "\n"
+    fileprivate static let _character: CharacterSet = CharacterSet.newlines
 
     static var stringRepresentation: String {
-        String(LineBreakCharacter._character)
+        return String(describing: _character)
+        //        String(LineBreakCharacter._character)
     }
 
     static var attributedStringRepresentation: NSAttributedString {
@@ -26,7 +28,8 @@ extension String {
         replacingOccurrences(of: target, with: LineBreakCharacter.stringRepresentation)
     }
 
-    func splitWithLineSeparator() -> [Substring] {
-        split(separator: LineBreakCharacter._character)
+    func splitWithLineSeparator() -> [String] {
+//        split(separator: LineBreakCharacter._character)
+        return components(separatedBy: LineBreakCharacter._character)
     }
 }
